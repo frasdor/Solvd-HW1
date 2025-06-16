9faf2d36b4dda18f9e50b10cf58d4c1d20d8dd4b
Dorota Frasunkiewicz <frasunkiewicz.dorota@gmail.com>
2025-06-16 11:16:59 +0200

    Add Homework 3 Task 1 : calculateTotalPrice

--- homework-03/task1.js
+++ homework-03/task1.js
@@ -1,3 +1,8 @@
+const products = [
+  { name: "Phone", price: 1500 },
+  { name: "Headphones", price: 300 }
+];
+
 function calculateDiscountedPrice (products, discount) {
     return products.map(product => {
         return {
@@ -6,10 +11,17 @@ function calculateDiscountedPrice (products, discount) {
     };
  }); 
 }
-const products = [
-  { name: "Phone", price: 1500 },
-  { name: "Headphones", price: 300 }
-];
+
+function calculateTotalPrice (products) {
+    let totalPrice = products.reduce((sum ,product) => {
+        return sum + product.price;
+    }, 0);
+    return totalPrice;
+}
+
 
 const discountedProducts = calculateDiscountedPrice(products, 20);
 console.log(discountedProducts);
+
+console.log(calculateTotalPrice(products)); 
+console.log(calculateTotalPrice(discountedProducts));
