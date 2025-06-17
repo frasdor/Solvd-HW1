78ede9547a56459a71a0976dfe7a69a1f6d609f2
Dorota Frasunkiewicz <frasunkiewicz.dorota@gmail.com>
2025-06-17 21:57:22 +0200

    Homework 3 task 3: add repeatFunction

--- homework-03/task3.js
+++ homework-03/task3.js
@@ -5,3 +5,23 @@ function createCounter(){
         return count;
     };
 }
+
+function repeatFunction (fn, times) {
+    return function() {
+        if (times < 0) {
+            const intervalId = setInterval (fn, 10);
+            return () => clearInterval(intervalId);
+        } else {
+            for (let i = 0; i < times; i++) {
+                fn();
+            }
+            return null;
+        }
+    };
+}
+
+
+const counter = createCounter();
+
+const repeatCount = repeatFunction(() => console.log(counter()), 5);
+repeatCount();
