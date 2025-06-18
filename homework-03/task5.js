4ae05d55be12f12449a45bfc904893d0d44328f5
Dorota Frasunkiewicz <frasunkiewicz.dorota@gmail.com>
2025-06-18 11:31:28 +0200

    Homework 3 task 5: add lazyMap function

--- homework-03/task5.js
+++ homework-03/task5.js
@@ -1,3 +1,32 @@
-function lazyMap(){
-    
+const dates = [
+  new Date(2025, 5, 17), 
+  new Date(2025, 5, 18), 
+  new Date(2025, 5, 19)  
+];
+
+function formatDate(date) {
+  return date.toDateString(); 
 }
+
+function lazyMap(array, mapFn){
+    let index = 0;
+
+    return {
+        next: function () {
+            if (index < array.length) {
+                const value = mapFn(array[index]);
+                index++;
+                return {value, done: false };
+            } else {
+                return { value: undefined, done: true };
+            }
+        }
+    };
+}
+
+const lazy = lazyMap(dates, formatDate);
+
+console.log(lazy.next());
+console.log(lazy.next());
+console.log(lazy.next());
+console.log(lazy.next());
